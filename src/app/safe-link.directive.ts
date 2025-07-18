8c5226c5b74642fbe5227d5ed6ed585536e5689f
carlosthompson <carlosethompson@gmail.com>
2025-07-18 16:41:00 +0100

    feat: first version of project

---
+++ src/app/safe-link.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, inject, input } from '@angular/core';
+
+// Decorator
+@Directive({
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
+})
+export class SafeLinkDirective {
+  queryParam = input('myapp', { alias: 'appSafeLink' });
+  private hostElement = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
+  constructor() {
+    console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+
+    if (wantsToLeave) {
+      const address = this.hostElement.nativeElement.href;
+      this.hostElement.nativeElement.href =
+        address + '?from=' + this.queryParam();
+      return;
+    }
+
+    event.preventDefault();
+  }
+}
